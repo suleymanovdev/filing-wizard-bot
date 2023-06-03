38f0cd32dba26d70017bfb6caafaabbd61d531a4
Elvin Suleymanov <97979588+suleymanovdev@users.noreply.github.com>
2023-06-03 15:42:17 +0400

    Version 1.0.1
    Added menu conversation with user.
    Added basic commands for manage files.

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
-# filing-wizard-bot
-This is hobby project for helping organize your files 😁
+# Filing Wizard Bot
+
+The Filing Wizard Bot is a Telegram bot. It allows users to manage files by uploading, deleting, and viewing them. This documentation provides an overview of the code and its functionality.
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+- [Functionality](#functionality)
+- [Usage](#usage)
+- [Error Handling](#error-handling)
+- [Dependencies](#dependencies)
+- [Feedback and Support](#feedback-and-support)
+
+## Getting Started
+
+To use the Filing Wizard Bot, follow these steps:
+
+1. Clone the repository or copy the code into your local project directory.
+2. Install the required dependencies by running `npm install`.
+3. Obtain a Telegram bot token by creating a new bot on Telegram's BotFather platform.
+4. Create a file named `bot_info.json` in the project directory and add the following content:
+
+```json
+{
+  "token": "YOUR_BOT_TOKEN"
+}
+```
+
+Replace YOUR_BOT_TOKEN with the token obtained from BotFather.
+
+Start the bot by running node <your_script_name>.js in the terminal.
+
+## Functionality
+
+The Filing Wizard Bot provides the following commands and features:
+
+* /start - Checks if a folder exists for the user and creates it if necessary. Sends a welcome message and displays the available commands.
+* /help - Displays the list of available commands.
+* /uploadfile - Initiates the file upload process.
+* /deletefile - Deletes a selected file from the user's folder.
+* /showallfiles - Displays a list of all files in the user's folder.
+
+## Usage
+
+### /start
+When a user sends the /start command, the bot checks if a folder exists for the user based on their chatId. If the folder doesn't exist, it creates it and sends a registration confirmation message. It then sends a welcome message with the available commands and displays a keyboard with command buttons.
+
+### /help
+The /help command sends a message to the user with information about the supported commands.
+
+### /showallfiles
+The /showallfiles command retrieves the list of files in the user's folder and sends a message to the user with the list.
+
+## File Upload
+When a user uploads a file, the bot listens for the 'document' event. It saves the file in the user's folder, renaming it if necessary, and sends a success message. It also displays the available commands keyboard.
+
+### /uploadfile
+When a user sends the /uploadfile command, the bot responds with a message requesting the user to upload the file they want to save.
+
+## File Deletion
+When the user selects a file to delete, the bot checks if the file exists in the user's folder. If it does, the bot deletes the file and sends a success message. It then displays the available commands keyboard.
+
+### /deletefile
+The /deletefile command initiates the file deletion process. The bot reads the files in the user's folder and sends a message with a keyboard containing buttons for each file. The user can select a file to delete.
+
+## Error Handling
+If any errors occur during file operations, such as file saving or deletion, an error message is sent to the user.
+
+## Dependencies
+The following dependencies are required to run the Filing Wizard Bot:
+
+* node-telegram-bot-api - provides the Telegram bot API for Node.js.
+* fs - provides file system-related functionality.
+* path - provides utilities for working with file and directory paths.
+
+These dependencies can be installed using npm install.
+
+
+## Feedback and Support
+If you have any questions, suggestions, or issues, please contact suleymanovdev.
---
+++ src/app.js
@@ -0,0 +1,201 @@
+// MIT License
+
+// Copyright (c) 2023 Elvin Suleymanov
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
+const path = require('path');
+const botInfo = require('./bot_info.json');
+
+const botToken = botInfo.token;
+const bot = new TelegramBot(botToken, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    const userFolder = `../db/${chatId}`;
+
+    if (!fs.existsSync(userFolder))
+    {
+        fs.mkdirSync(userFolder, { recursive: true });
+        bot.sendMessage(chatId, 'You are registered ✅');
+    }
+
+    const startMessage = `Welcome to the Filing Wizard Bot 😉\nbot developed by suleymanovdev.\n\n📓 Available commands:\n/help - Display the list of commands.\n/uploadfile - Upload a file.\n/deletefile - Delete a file.\n/showallfiles - Show all files.`;
+    const keyboard = [
+        [
+            { text: '/help' },
+            { text: '/uploadfile' },
+        ],
+        [
+            { text: '/deletefile' },
+            { text: '/showallfiles' },
+        ],
+    ];
+    const replyMarkup = {
+    keyboard,
+    resize_keyboard: true,
+    one_time_keyboard: false,
+    };
+
+    bot.sendMessage(chatId, startMessage, { reply_markup: replyMarkup });
+});
+
+bot.onText(/\/help/, (msg) => {
+    const chatId = msg.chat.id;
+    const helpMessage = 'You can upload only documents (such as .pdf, .zip or etc.) ❗\n\n📔 List of commands:\n/start - Check if a folder exists and create it if necessary.\n/help - Display the list of commands.\n/uploadfile - Upload a file.\n/deletefile - Delete a file.\n/showallfiles - Show all files.';
+    bot.sendMessage(chatId, helpMessage);
+});
+
+bot.onText(/\/uploadfile/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Please upload the file you want to save ✌️');
+});
+
+bot.on('document', (msg) => {
+    const chatId = msg.chat.id;
+    const userFolder = `../db/${chatId}`;
+
+    if (!fs.existsSync(userFolder))
+    {
+        fs.mkdirSync(userFolder, { recursive: true });
+        bot.sendMessage(chatId, 'You are registered ✅');
+    }
+
+    const fileId = msg.document.file_id;
+    const fileName = msg.document.file_name;
+    const filePath = path.join(userFolder, fileName);
+
+    bot.downloadFile(fileId, userFolder)
+    .then((filePath) => {
+        const originalFilePath = path.join(filePath);
+        const newFilePath = path.join(userFolder, fileName);
+        fs.renameSync(originalFilePath, newFilePath);
+        bot.sendMessage(chatId, `File ${fileName} saved ✅`);
+        const keyboard = [
+            [
+                { text: '/help' },
+                { text: '/uploadfile' },
+            ],
+            [
+                { text: '/deletefile' },
+                { text: '/showallfiles' },
+            ],
+        ];
+        const replyMarkup = {
+            keyboard,
+            resize_keyboard: true,
+            one_time_keyboard: false,
+        };
+        bot.sendMessage(chatId, 'Successful ✅', { reply_markup: replyMarkup });
+    })
+    .catch((error) => {
+        console.error(error);
+        bot.sendMessage(chatId, 'An error occurred while saving the file 📛');
+    });
+});
+
+bot.onText(/\/deletefile/, (msg) => {
+    const chatId = msg.chat.id;
+    const userFolder = `../db/${chatId}`;
+
+    fs.readdir(userFolder, (err, files) => {
+        if (err)
+        {
+            console.error(err);
+            bot.sendMessage(chatId, 'An error occurred while retrieving the files 📛');
+            return;
+        }
+
+        if (files.length === 0)
+        {
+            bot.sendMessage(chatId, 'No files found ❗');
+            return;
+        }
+
+        const fileButtons = files.map((file) => [file]);
+        const replyMarkup = {
+            keyboard: fileButtons,
+            resize_keyboard: true,
+            one_time_keyboard: true,
+        };
+
+        bot.sendMessage(chatId, 'Select the file you want to delete:', { reply_markup: replyMarkup });
+    });
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const userFolder = `../db/${chatId}`;
+
+    if (msg.text && fs.existsSync(path.join(userFolder, msg.text)))
+    {
+        fs.unlink(path.join(userFolder, msg.text), (err) => {
+            if (err)
+            {
+                console.error(err);
+                bot.sendMessage(chatId, 'An error occurred while deleting the file 📛');
+            }
+            else
+            {
+                bot.sendMessage(chatId, `File ${msg.text} deleted ✅`);
+                const keyboard = [
+                    [
+                        { text: '/help' },
+                        { text: '/uploadfile' },
+                    ],
+                    [
+                        { text: '/deletefile' },
+                        { text: '/showallfiles' },
+                    ],
+                ];
+                const replyMarkup = {
+                keyboard,
+                resize_keyboard: true,
+                one_time_keyboard: false,
+                };
+                bot.sendMessage(chatId, 'Successful ✅', { reply_markup: replyMarkup });
+            }
+        });
+    }
+});
+
+bot.onText(/\/showallfiles/, (msg) => {
+    const chatId = msg.chat.id;
+    const userFolder = `../db/${chatId}`;
+
+    fs.readdir(userFolder, (err, files) => {
+        if (err)
+        {
+            console.error(err);
+            bot.sendMessage(chatId, 'An error occurred while retrieving the files 📛');
+            return;
+        }
+
+        if (files.length === 0)
+        {
+            bot.sendMessage(chatId, 'No files found ❗');
+            return;
+        }
+
+        const fileList = files.join('\n');
+        bot.sendMessage(chatId, `List of files:\n${fileList}`);
+    });
+});
